c1dfa38b3b3739a0c0ccf5b29b3df63bd2419ee1
Jobin Biju <jobinbiju9090@gmail.com>
2021-07-11 23:06:38 +0530

    Removed http & made express only

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-import { createServer } from 'http';
-
 import express from 'express';
 
 const app = express();
@@ -11,10 +9,9 @@ app.use((req, res, next) => {
 
 app.use((req, res, next) => {
   console.log('In the another middleware');
-  
+
   res.send('<h1>Hello from express</h1>');
 });
 
-const server = createServer(app);
 
-server.listen(3000);
+app.listen(3000);
