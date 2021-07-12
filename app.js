2a35318de3a0e7fdfbcb82fd7906557c8e4b365e
Jobin Biju <jobinbiju9090@gmail.com>
2021-07-12 23:45:49 +0530

    Serving 404 html page

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import path from 'path';
 
 import * as adminRoutes from './routes/admin.js';
 import * as shopRoutes from './routes/shop.js';
 
 const app = express();
 const port = 3000;
+const __dirname = path.resolve();
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -12,7 +14,7 @@ app.use('/admin', adminRoutes.default);
 app.use(shopRoutes.default);
 
 app.use((req, res, next) => {
-  res.status(404).send('<H1>Page not found</H1>');
+  res.status(404).sendFile(path.join(__dirname, './', 'views', '404.html'));
 });
 
 app.listen(port);
