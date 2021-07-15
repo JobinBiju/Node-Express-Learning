e4c489bd7c189ea8fa1e57d1db96cbb08f12d58a
Jobin Biju <jobinbiju9090@gmail.com>
2021-07-15 23:29:26 +0530

    Styled 404.html

--- routes/admin.js
+++ routes/admin.js
@@ -3,9 +3,7 @@ import path from 'path';
 
 const router = express.Router();
 
-
 router.get('/add-product', (req, res, next) => {
-  console.log('Just another middleware');
   res.sendFile(path.join(appRoot, 'views', 'add-product.html'));
 });
 router.post('/add-product', (req, res, next) => {
