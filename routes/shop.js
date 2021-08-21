c471cbbdf14b0bbea37ba4562cc34625baa888b1
Jobin Biju <jobinbiju9090@gmail.com>
2021-08-21 16:26:53 +0530

    Completed with layouts in handlebars

--- routes/shop.js
+++ routes/shop.js
@@ -6,7 +6,14 @@ import * as admin from '../routes/admin.js';
 const router = express.Router();
 router.get('/', (req, res, next) => {
   const product = admin.products;
-  res.render('shop', { prods: product, pageTitle: 'My E-Shop', path: '/shop' });
+  res.render('shop', {
+    prods: product,
+    pageTitle: 'My E-Shop',
+    path: '/shop',
+    hasProducts: product.length > 0,
+    activeShop: true,
+    productCSS: true,
+  });
   // res.sendFile(path.join(appRoot, 'views', 'shop.html'));
   console.log(admin.products);
 });
