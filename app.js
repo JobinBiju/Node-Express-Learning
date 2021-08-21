ed38f68991eff4e0520d2c0a61d85840def52567
Jobin Biju <jobinbiju9090@gmail.com>
2021-08-21 16:42:57 +0530

    Added EJS templates

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import path from 'path';
-import expressHbs from 'express-handlebars';
 
 import * as admin from './routes/admin.js';
 import * as shop from './routes/shop.js';
@@ -9,15 +8,8 @@ const app = express();
 const port = 3000;
 global.appRoot = path.resolve();
 
-app.engine(
-  'hbs',
-  expressHbs({
-    layoutsDir: 'views/layouts/',
-    defaultLayout: 'main-layout',
-    extname: 'hbs',
-  }),
-);
-app.set('view engine', 'hbs'); // setting handlebars as default template engine
+
+app.set('view engine', 'ejs'); // setting handlebars as default template engine
 app.set('views', 'views'); // location to find the views
 
 app.use(express.urlencoded({ extended: false }));
